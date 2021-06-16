5d73a6cda81d6c030e6cd04f22b28423a973355b
TAiGA <>
2021-06-16 21:44:52 +0800

    Use WGL to EGL for Windows

--- third_party/EGL/egl_windows.cc
+++ third_party/EGL/egl_windows.cc
@@ -10,18 +10,19 @@
 ** Khronos $Git commit SHA1: 59522adade $ on $Git commit date: 2021-02-02 11:09:11 -0700 $
 */
 
+#define _GDI32_
 #include <windows.h>
 #include <EGL/egl.h>
 
-#define EGL_PROTOTYPE(type, prototype, parameter, ...) \
-extern type (EGLAPIENTRYP prototype ## Entry) parameter; \
-extern "C" type EGLAPIENTRY prototype parameter \
+#define WGL_PROTOTYPE(type, prototype, parameter, ...) \
+extern type (WINAPI* prototype ## Entry) parameter; \
+extern "C" type WINAPI prototype parameter \
 { \
     return prototype ## Entry(__VA_ARGS__); \
 } \
-static type EGLAPIENTRY prototype ## Trunk parameter \
+static type WINAPI prototype ## Trunk parameter \
 { \
-    (void*&)prototype ## Entry = GetProcAddress(LoadLibraryA("libEGL.dll"), #prototype); \
+    (void*&)prototype ## Entry = GetProcAddress(LoadLibraryA("OpenGL32.dll"), #prototype); \
     if (prototype ## Entry == nullptr) \
     { \
         prototype ## Entry = [] parameter -> type \
@@ -32,63 +33,119 @@ static type EGLAPIENTRY prototype ## Trunk parameter \
     } \
     return prototype ## Entry(__VA_ARGS__); \
 } \
-type (EGLAPIENTRYP prototype ## Entry) parameter = prototype ## Trunk;
+type (WINAPI* prototype ## Entry) parameter = prototype ## Trunk;
 
-#if EGL_VERSION_1_0
-EGL_PROTOTYPE(EGLBoolean, eglChooseConfig, (EGLDisplay dpy, const EGLint *attrib_list, EGLConfig *configs, EGLint config_size, EGLint *num_config), dpy, attrib_list, configs, config_size, num_config);
-EGL_PROTOTYPE(EGLBoolean, eglCopyBuffers, (EGLDisplay dpy, EGLSurface surface, EGLNativePixmapType target), dpy, surface, target);
-EGL_PROTOTYPE(EGLContext, eglCreateContext, (EGLDisplay dpy, EGLConfig config, EGLContext share_context, const EGLint *attrib_list), dpy, config, share_context, attrib_list);
-EGL_PROTOTYPE(EGLSurface, eglCreatePbufferSurface, (EGLDisplay dpy, EGLConfig config, const EGLint *attrib_list), dpy, config, attrib_list);
-EGL_PROTOTYPE(EGLSurface, eglCreatePixmapSurface, (EGLDisplay dpy, EGLConfig config, EGLNativePixmapType pixmap, const EGLint *attrib_list), dpy, config, pixmap, attrib_list);
-EGL_PROTOTYPE(EGLSurface, eglCreateWindowSurface ,(EGLDisplay dpy, EGLConfig config, EGLNativeWindowType win, const EGLint *attrib_list), dpy, config, win, attrib_list);
-EGL_PROTOTYPE(EGLBoolean, eglDestroyContext, (EGLDisplay dpy, EGLContext ctx), dpy, ctx);
-EGL_PROTOTYPE(EGLBoolean, eglDestroySurface, (EGLDisplay dpy, EGLSurface surface), dpy, surface);
-EGL_PROTOTYPE(EGLBoolean, eglGetConfigAttrib, (EGLDisplay dpy, EGLConfig config, EGLint attribute, EGLint *value), dpy, config, attribute, value);
-EGL_PROTOTYPE(EGLBoolean, eglGetConfigs, (EGLDisplay dpy, EGLConfig *configs, EGLint config_size, EGLint *num_config), dpy, configs, config_size, num_config);
-EGL_PROTOTYPE(EGLDisplay, eglGetCurrentDisplay, (void));
-EGL_PROTOTYPE(EGLSurface, eglGetCurrentSurface, (EGLint readdraw), readdraw);
-EGL_PROTOTYPE(EGLDisplay, eglGetDisplay, (EGLNativeDisplayType display_id), display_id);
-EGL_PROTOTYPE(EGLint, eglGetError, (void));
-EGL_PROTOTYPE(__eglMustCastToProperFunctionPointerType, eglGetProcAddress, (const char *procname), procname);
-EGL_PROTOTYPE(EGLBoolean, eglInitialize, (EGLDisplay dpy, EGLint *major, EGLint *minor), dpy, major, minor);
-EGL_PROTOTYPE(EGLBoolean, eglMakeCurrent, (EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx), dpy, draw, read, ctx);
-EGL_PROTOTYPE(EGLBoolean, eglQueryContext, (EGLDisplay dpy, EGLContext ctx, EGLint attribute, EGLint *value), dpy, ctx, attribute, value);
-EGL_PROTOTYPE(const char *, eglQueryString, (EGLDisplay dpy, EGLint name), dpy, name);
-EGL_PROTOTYPE(EGLBoolean, eglQuerySurface, (EGLDisplay dpy, EGLSurface surface, EGLint attribute, EGLint *value), dpy, surface, attribute, value);
-EGL_PROTOTYPE(EGLBoolean, eglSwapBuffers, (EGLDisplay dpy, EGLSurface surface), dpy, surface);
-EGL_PROTOTYPE(EGLBoolean, eglTerminate, (EGLDisplay dpy), dpy);
-EGL_PROTOTYPE(EGLBoolean, eglWaitGL, (void));
-EGL_PROTOTYPE(EGLBoolean, eglWaitNative, (EGLint engine), engine);
-#endif /* EGL_VERSION_1_0 */
+WGL_PROTOTYPE(HGLRC, wglCreateContext, (HDC dc), dc);
+WGL_PROTOTYPE(BOOL, wglDeleteContext, (HGLRC glrc), glrc);
+WGL_PROTOTYPE(HGLRC, wglGetCurrentContext, (VOID));
+WGL_PROTOTYPE(HDC, wglGetCurrentDC, (VOID));
+WGL_PROTOTYPE(PROC, wglGetProcAddress, (LPCSTR name), name);
+WGL_PROTOTYPE(BOOL, wglMakeCurrent, (HDC dc, HGLRC glrc), dc, glrc);
+WGL_PROTOTYPE(BOOL, wglShareLists, (HGLRC share, HGLRC glrc), share, glrc);
 
-#if EGL_VERSION_1_1
-EGL_PROTOTYPE(EGLBoolean, eglBindTexImage, (EGLDisplay dpy, EGLSurface surface, EGLint buffer), dpy, surface, buffer);
-EGL_PROTOTYPE(EGLBoolean, eglReleaseTexImage, (EGLDisplay dpy, EGLSurface surface, EGLint buffer), dpy, surface, buffer);
-EGL_PROTOTYPE(EGLBoolean, eglSurfaceAttrib, (EGLDisplay dpy, EGLSurface surface, EGLint attribute, EGLint value), dpy, surface, attribute, value);
-EGL_PROTOTYPE(EGLBoolean, eglSwapInterval, (EGLDisplay dpy, EGLint interval), dpy, interval);
-#endif /* EGL_VERSION_1_1 */
+EGLBoolean eglInitialize(EGLDisplay dpy, EGLint *major, EGLint *minor)
+{
+    if (major)
+        (*major) = 1;
+    if (minor)
+        (*major) = 5;
+    return EGL_TRUE;
+}
 
-#if EGL_VERSION_1_2
-EGL_PROTOTYPE(EGLBoolean, eglBindAPI, (EGLenum api), api);
-EGL_PROTOTYPE(EGLenum, eglQueryAPI, (void));
-EGL_PROTOTYPE(EGLSurface, eglCreatePbufferFromClientBuffer, (EGLDisplay dpy, EGLenum buftype, EGLClientBuffer buffer, EGLConfig config, const EGLint *attrib_list), dpy, buftype, buffer, config, attrib_list);
-EGL_PROTOTYPE(EGLBoolean, eglReleaseThread, (void));
-EGL_PROTOTYPE(EGLBoolean, eglWaitClient, (void));
-#endif /* EGL_VERSION_1_2 */
+EGLBoolean eglChooseConfig(EGLDisplay dpy, const EGLint *attrib_list, EGLConfig *configs, EGLint config_size, EGLint *num_config)
+{
+    return EGL_TRUE;
+}
 
-#if EGL_VERSION_1_4
-EGL_PROTOTYPE(EGLContext, eglGetCurrentContext, (void));
-#endif /* EGL_VERSION_1_4 */
+EGLBoolean eglBindAPI(EGLenum api)
+{
+    return EGL_TRUE;
+}
 
-#if EGL_VERSION_1_5
-EGL_PROTOTYPE(EGLSync, eglCreateSync, (EGLDisplay dpy, EGLenum type, const EGLAttrib *attrib_list), dpy, type, attrib_list);
-EGL_PROTOTYPE(EGLBoolean, eglDestroySync, (EGLDisplay dpy, EGLSync sync), dpy, sync);
-EGL_PROTOTYPE(EGLint, eglClientWaitSync, (EGLDisplay dpy, EGLSync sync, EGLint flags, EGLTime timeout), dpy, sync, flags, timeout);
-EGL_PROTOTYPE(EGLBoolean, eglGetSyncAttrib, (EGLDisplay dpy, EGLSync sync, EGLint attribute, EGLAttrib *value), dpy, sync, attribute, value);
-EGL_PROTOTYPE(EGLImage, eglCreateImage, (EGLDisplay dpy, EGLContext ctx, EGLenum target, EGLClientBuffer buffer, const EGLAttrib *attrib_list), dpy, ctx, target, buffer, attrib_list);
-EGL_PROTOTYPE(EGLBoolean, eglDestroyImage, (EGLDisplay dpy, EGLImage image), dpy, image);
-EGL_PROTOTYPE(EGLDisplay, eglGetPlatformDisplay, (EGLenum platform, void *native_display, const EGLAttrib *attrib_list), platform, native_display, attrib_list);
-EGL_PROTOTYPE(EGLSurface, eglCreatePlatformWindowSurface, (EGLDisplay dpy, EGLConfig config, void *native_window, const EGLAttrib *attrib_list), dpy, config, native_window, attrib_list);
-EGL_PROTOTYPE(EGLSurface, eglCreatePlatformPixmapSurface, (EGLDisplay dpy, EGLConfig config, void *native_pixmap, const EGLAttrib *attrib_list), dpy, config, native_pixmap, attrib_list);
-EGL_PROTOTYPE(EGLBoolean, eglWaitSync, (EGLDisplay dpy, EGLSync sync, EGLint flags), dpy, sync, flags);
-#endif /* EGL_VERSION_1_5 */
+const char *eglQueryString(EGLDisplay dpy, EGLint name)
+{
+    switch (name)
+    {
+    case EGL_CLIENT_APIS:
+        return "OpenGL_ES";
+    case EGL_VENDOR:
+        return "TensorFlow";
+    case EGL_VERSION:
+        return "1.5";
+    default:
+        return "";
+    }
+}
+
+EGLint eglGetError(void)
+{
+    return 0;
+}
+
+__eglMustCastToProperFunctionPointerType eglGetProcAddress(const char *procname)
+{
+    return (__eglMustCastToProperFunctionPointerType)wglGetProcAddress(procname);
+}
+
+EGLContext eglGetCurrentContext(void)
+{
+    return wglGetCurrentContext();
+}
+
+EGLContext eglCreateContext(EGLDisplay dpy, EGLConfig config, EGLContext share_context, const EGLint *attrib_list)
+{
+    HGLRC ctx = wglCreateContext((HDC)dpy);
+    if (ctx && share_context)
+        wglShareLists((HGLRC)share_context, ctx);
+    return ctx;
+}
+
+EGLBoolean eglDestroyContext(EGLDisplay dpy, EGLContext ctx)
+{
+    return wglDeleteContext((HGLRC)ctx) ? EGL_TRUE : EGL_FALSE;
+}
+
+EGLBoolean eglMakeCurrent(EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx)
+{
+    return wglMakeCurrent((HDC)dpy, (HGLRC)ctx);
+}
+
+EGLDisplay eglGetCurrentDisplay(void)
+{
+    return (EGLDisplay)wglGetCurrentDC();
+}
+
+EGLDisplay eglGetDisplay(EGLNativeDisplayType display_id)
+{
+    if (display_id == EGL_DEFAULT_DISPLAY)
+    {
+        static HWND window = nullptr;
+        static HDC dc = nullptr;
+        if (window == nullptr)
+        {
+            WNDCLASSEXA wc = { sizeof(WNDCLASSEXA), CS_OWNDC, ::DefWindowProcA, 0, 0, ::GetModuleHandleA(nullptr), nullptr, nullptr, nullptr, nullptr, "OpenGL", nullptr };
+            ::RegisterClassExA(&wc);
+            window = ::CreateWindowA(wc.lpszClassName, "OpenGL", WS_OVERLAPPEDWINDOW, 0, 0, 1, 1, nullptr, nullptr, wc.hInstance, nullptr);
+            dc = GetDC(window);
+        }
+        return dc;
+    }
+    return (EGLDisplay)display_id;
+}
+
+EGLSurface eglCreatePbufferSurface(EGLDisplay dpy, EGLConfig config, const EGLint *attrib_list)
+{
+    static const PIXELFORMATDESCRIPTOR desc =
+    {
+        .nSize = sizeof(PIXELFORMATDESCRIPTOR),
+        .nVersion = 1,
+        .dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+    };
+    SetPixelFormat((HDC)dpy, ChoosePixelFormat((HDC)dpy, &desc), &desc);
+    return (EGLSurface)dpy;
+}
+
+EGLBoolean eglDestroySurface(EGLDisplay dpy, EGLSurface surface)
+{
+    return EGL_TRUE;
+}
